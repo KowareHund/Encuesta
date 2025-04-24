71c69670c02614d13c34f3bfa68725e9c7ef0c5d
JuanOsorioo <josoriogon@unal.edu.co>
2025-04-23 21:08:25 -0400

    Update1.1 Encuesta_Clase.java
    Se añaden comentarios explicando parte del codigo y el primer cuadro de resultados sin cambiar valores

--- OneDrive/Documentos/NetBeansProjects/Encuesta_Clase/src/encuesta_clase/Encuesta_Clase.java
+++ OneDrive/Documentos/NetBeansProjects/Encuesta_Clase/src/encuesta_clase/Encuesta_Clase.java
@@ -3,7 +3,7 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
  */
 package encuesta_clase;
-import java.util.Scanner;
+import java.util.Scanner;   //Se importa el Scanner para usarlo
 /**
  *
  * @author Esteban Ricardo y Juan Osorio
@@ -15,23 +15,29 @@ public class Encuesta_Clase {
      */
     public static void main(String[] args) {
         
-        Scanner Datos = new Scanner(System.in);
+        Scanner Datos = new Scanner(System.in);    //Se inicia el Scanner con el nombre Datos
         
-        System.out.println("Ingrese su nombre");
+        System.out.println("Ingrese su nombre");    //Se piden y guardan los diferentes tipos de datos
         String nombre = Datos.nextLine();
         
         System.out.println("Ingrese su carrera");
         String carrera = Datos.nextLine();
         
         System.out.println("Ingrese su edad");
-        Integer edad = Integer.valueOf(Datos.nextLine());
+        Integer edad = Integer.valueOf(Datos.nextLine());    
         
         System.out.println("Ingrese su numero");
-        Long numero = Long.valueOf(Datos.nextLine());
+        Long numero = Long.valueOf(Datos.nextLine());    //Se coloca long puesto que el telefono supera buffer del int 
 
+        System.out.println("--------------------------------------------------");
+        System.out.println("|             Datos antes del cambio             |");   
+        System.out.println("|     Su nombre es: "+ nombre );           //Se muestran los resultados obtenidos
+        System.out.println("|     Su carrera es: "+ carrera );
+        System.out.println("|     Su edad es: "+ edad );
+        System.out.println("|     Su telefono es: "+ numero );
+        System.out.println("--------------------------------------------------");
 
-
-       
+        
     }
     
 }
